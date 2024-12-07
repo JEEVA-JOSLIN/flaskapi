d5f2ebe887ce839c9ec09576a420a73ff96e70af
Jeeva Joslin <108604763+JEEVA-JOSLIN@users.noreply.github.com>
2024-12-08 02:31:47 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,6 @@
 apt-get update
 apt-get install -y libmagic1
+apt-get install build-essential
+apt-get install python3-dev
+apt-get install libjpeg-dev
 gunicorn --workers 3 --bind 0.0.0.0:8000 app:app
