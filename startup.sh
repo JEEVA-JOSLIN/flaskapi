e9c4180cf5ce83c7780b3f5c49218e120ba39f1d
Jeeva Joslin <108604763+JEEVA-JOSLIN@users.noreply.github.com>
2024-12-08 10:36:32 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@ apt-get install build-essential
 apt-get install python3-dev
 apt-get install libjpeg-dev
 apt-get install -y libmupdf-dev mupdf-tools
-gunicorn --workers 3 --bind 0.0.0.0:8000 app:app
+gunicorn --workers 3 --bind 0.0.0.0:8000 app:app --log-level debug
